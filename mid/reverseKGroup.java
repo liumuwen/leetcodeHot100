fcf0086ce827e912ef2f82b4440dbb6dcb7fbd1b
青青 <3835904494@qq.com>
2025-11-12 10:27:10 +0800

    k个一组反转链表

---
+++ mid/reverseKGroup.java
@@ -0,0 +1,64 @@
+package mid;
+import java.util.*;
+public class reverseKGroup {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+    public static void reverse(ListNode startPre, ListNode end){
+        ListNode pre = end.next;
+        ListNode next = end.next;
+        ListNode cur = startPre.next;
+
+        //这里不能直接写cur != end.next 因为他是变化的
+        while(cur != next){
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        startPre.next = pre;
+    }
+    public static ListNode reverseKGroup(ListNode head,int k){
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode cur = dummy;
+        while(cur != null){
+            ListNode startPre = cur;
+            for(int i = 0;i < k && cur != null;i++){
+                cur = cur.next;
+            }
+            ListNode tmp = startPre.next;
+            if(cur != null){
+                reverse(startPre,cur);
+                cur = tmp;
+            }
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String[] split = s.split(",");
+        ListNode head = new ListNode(Integer.parseInt(split[0]));
+        ListNode cur = head;
+        for (int i = 1; i < split.length; i++) {
+            cur.next = new ListNode(Integer.parseInt(split[i]));
+            cur = cur.next;
+        }
+        int k = sc.nextInt();
+        ListNode node = reverseKGroup(head, k);
+        while (node != null){
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+}
